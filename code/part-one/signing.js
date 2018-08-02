083bf9590c15b48f9bb11c7cf59b195a306f962c
zackcarlson <zackt.carlson@gmail.com>
2018-08-01 20:29:34 -0700

    Create a private hex key

--- code/part-one/signing.js
+++ code/part-one/signing.js
@@ -14,8 +14,11 @@ const { randomBytes, createHash } = require('crypto');
  *   // 'e291df3eede7f0c520fddbe5e9e53434ff7ef3c0894ed9d9cbcb6596f1cfe87e'
  */
 const createPrivateKey = () => {
-  // Enter your solution here
-
+  let privKey;
+  do {
+    privKey = randomBytes(32);
+  } while (!secp256k1.privateKeyVerify(privKey));
+  return privKey.toString('hex');
 };
 
 /**
